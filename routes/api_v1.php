108138f8d2018df189acdd2558d3eed46ce98df1
Gehenna63 <gehenna63@hotmail.com>
2026-03-25 10:37:00 -0400

    Refactor api route with apiResource

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -5,9 +5,5 @@ use App\Http\Controllers\Api\V1\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('authors', AuthorController::class);
-//Route::post('/authors', [AuthorController::class, 'store']);
-//Route::get('/authors', [AuthorController::class, 'index']);
 
-Route::post('/books', [BookController::class, 'store']);
-Route::get('/books', [BookController::class, 'index']);
-Route::get('/books/{uuid}', [BookController::class, 'show']);
+Route::apiResource('books', BookController::class);
